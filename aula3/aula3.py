b56207bbd888380be5a8665d011215001ff379da
Geison <gs_gaara@hotmail.com>
2020-08-09 16:47:39 -0300

    aula string

---
+++ aula3/aula3.py
@@ -0,0 +1,9 @@
+"""
+str - string
+"""
+
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str).')
+print("Essa é uma \"string\" (str).")  # aqui foi usado um caractere de escape, onde o \ vai ignorar as aspas em seguida
+print('Esse é meu \n (str).')  # cuidado porque o \ também é lido pelo interpretador do python e pode gerar um comando
+print(r'Esse é meu \n (str).')  # o r usado antes da string diz pro interpretador que não é pra executar nada dentro da string
