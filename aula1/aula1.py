58a2637cd73f079e5b31464b2301568f8ab6591e
Geison <gs_gaara@hotmail.com>
2020-08-09 16:05:13 -0300

    aula nova comentários

---
+++ aula1/aula1.py
@@ -0,0 +1,18 @@
+# Comentário
+print('Linha 1')  # outro comentário
+print('Linha 2')  # outro
+# Falando sobre do que se trata essa linha
+# Outra linha
+"""
+print('Linha 3')
+print('Linha 4')  # Isso é um comentário
+print('Linha 5')  # mais um
+print('Linha 6')
+# Final
+"""
+
+# De acordo com PEP 8 os comentários devem ter dois espaços antes da cerquilha
+
+"""
+Tres aspas podem também serem usadas para fazer comentários ou documentações
+"""
