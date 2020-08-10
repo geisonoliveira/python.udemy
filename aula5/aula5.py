feb5a251c612e8a60b91575c3850331f78bda892
Geison <gs_gaara@hotmail.com>
2020-08-09 21:48:25 -0300

    operadores numericos

---
+++ aula5/aula5.py
@@ -0,0 +1,22 @@
+"""
++ = adiçao
+- = subtração
+* = multiplicação
+/ = divisão comum, com ponto flutuante
+// = divisão inteira, arredondado
+** = exponenciação, um número elevado a outro
+% = módulo/resto da divisão
+() = alterar precedência
+"""
+
+print('Adição + ', 10 + 10)
+print('Subtração - ', 10 - 9)
+print('Multiplicação * ', 10 * 10)
+print('Divisão / ', 10 / 2)
+
+print('Repetição *', 10 * '10')  # O * fez uma repetição da string '10'
+print('Concatenação +', '5' + '5')  # O + fez uma concatenação com duas strings
+
+print(10 // 3)
+print(2 ** 10)
+print(10 % 3)
